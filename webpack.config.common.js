b821240843c20c93f869071d57442b29cda9c6c1
vitalyvitmens <otm_@tut.by>
2023-12-27 22:59:48 +0300

    feat(webpack.config.common.js, webpack.config.dev.js, webpack.config.prod.js): добавляет файлы

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -8,6 +10,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/favicon.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
 }
 
 // const path = require('path')
